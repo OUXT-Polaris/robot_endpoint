9b7f5ac8ff2d1d446ecc40c9f92936e4435b59d0
Masaya Kataoka <ms.kataoka@gmail.com>
2021-02-27 23:58:29 +0900

    initial commit
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='robot_endpoint',
+    version="0.0.1",
+    description="FOTA tools for ROS/ROS2 packages",
+    long_description="",
+    author='masaya kataoka',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ]
+    install_requires=[
+        "boto3",
+        "awsiotsdk"
+    ],
+    ,packages=['package_synchronizer']
+)
