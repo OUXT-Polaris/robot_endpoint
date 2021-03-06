0a4e3442700de43673f4c3813aaca6703c8469b7
Masaya Kataoka <ms.kataoka@gmail.com>
2021-03-06 20:15:43 +0900

    add option
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- robot_endpoint/robot_endpoint.py
+++ robot_endpoint/robot_endpoint.py
@@ -4,15 +4,9 @@ import argparse
 import time
 from fastapi import FastAPI
 
-from pydantic import BaseModel
-from typing import Optional, List
-
-class SynchronizePackagesParameters(BaseModel):
-    force_update: False
-
 app = FastAPI()
 
-@app.post('/')
-async def synchronize_packages(params: SynchronizePackagesParameters):
+@app.get('/get/{synchronize_packages}')
+async def synchronize_packages(force_update: bool):
     sync = PackageSynchronizer("../example_config.yaml")
-    return sync.synchronize()
+    return {"result" : sync.synchronize(force_update)}
