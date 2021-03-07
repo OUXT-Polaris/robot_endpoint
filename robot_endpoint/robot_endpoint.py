df0bc15060317e48d19eeb859e5c261707010718
Masaya Kataoka <ms.kataoka@gmail.com>
2021-03-07 15:35:55 +0900

    enable launch
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- robot_endpoint/robot_endpoint.py
+++ robot_endpoint/robot_endpoint.py
@@ -5,13 +5,47 @@ import time
 from fastapi import FastAPI
 import os
 import signal
-from subprocess import Popen, PIPE
+import launch
+from launch.frontend import Parser
+from ament_index_python.packages import get_package_share_directory
+
+from typing import List
+from typing import Text
+from typing import Tuple
 
 class ProcessManager:
+    def parse_launch_arguments(launch_arguments: List[Text]) -> List[Tuple[Text, Text]]:
+        """Parse the given launch arguments from the command line, into list of tuples for launch."""
+        parsed_launch_arguments = OrderedDict()  # type: ignore
+        for argument in launch_arguments:
+            count = argument.count(':=')
+            if count == 0 or argument.startswith(':=') or (count == 1 and argument.endswith(':=')):
+                raise RuntimeError(
+                    "malformed launch argument '{}', expected format '<name>:=<value>'"
+                    .format(argument))
+            name, value = argument.split(':=', maxsplit=1)
+            parsed_launch_arguments[name] = value  # last one wins is intentional
+        return parsed_launch_arguments.items()
     def call(self, package_name, launch_filename):
-        self.p = Popen(['ros2', 'launch', package_name, launch_filename], shell=False)
+        launch_file_path = os.path.join(get_package_share_directory(package_name), 'launch', launch_filename)
+        self.launch_service = launch.LaunchService()
+            # argv=launch_file_arguments,
+            # noninteractive=False,
+            #debug=False)
+        # parsed_launch_arguments = self.parse_launch_arguments(launch_file_arguments)
+        launch_description = launch.LaunchDescription([
+            launch.actions.IncludeLaunchDescription(
+                launch.launch_description_sources.AnyLaunchDescriptionSource(
+                    launch_file_path
+                ),
+                # launch_arguments=parsed_launch_arguments,
+            ),
+        ])
+        self.launch_service.include_launch_description(launch_description)
+        ret = self.launch_service.run()
     def terminate(self):
-        os.kill(self.p.pid, signal.SIGINT)
+        pass
+        # os.kill(self.p.pid, signal.SIGINT)
         # self.p.kill()
 
 proc_manager = ProcessManager()
